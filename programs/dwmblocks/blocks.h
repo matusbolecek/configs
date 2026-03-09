0801202982251a87cb45bb5e1baf5ad8645d961f
matusbolecek <matus.bolecek@gmail.com>
2026-03-09 12:55:41 +0100

    cleanup and reorganized

---
+++ programs/dwmblocks/blocks.h
@@ -0,0 +1,22 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/		/*Command*/	 		/*Update Interval*/	/*Update Signal*/
+//  	{"", 		".local/bin/sb-scripts/music",	  		60,		        4},
+//
+    {" ", ".local/bin/sb-scripts/timer.sh",              30,       8     },
+
+  	{" ", 		".local/bin/sb-scripts/kernel",	  	0,		        5},
+
+//	{" ", 		".local/bin/sb-scripts/cputemp",	        2,		        6},
+//
+	{" ", 		".local/bin/sb-scripts/memory",	        2,		        7},
+
+	{" 🔊 ", 	".local/bin/sb-scripts/volume",		2,		        10},
+
+	{" ", 		".local/bin/sb-scripts/kbdlayout",		0,		        20},
+	
+	{" ", 		".local/bin/sb-scripts/clock",			5,		        30},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = '|';
--- redundant/stow-redundant/autostart/.dwm/autostart.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-picom --config ~/.config/picom/picom.conf --experimental-backend &
-nitrogen --random --set-zoom-fill &
-lxsession &
-dwmblocks &
-dunst &
